65fc7daeaa24b9d5a5e15adb02d6229cc7de8eb8
Yakovyakov <yyak0423@gmail.com>
2025-03-10 15:17:15 -0400

    level 0,  first test refactoring

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,17 +1,21 @@
 export class Board {
   width;
   height;
+  immobile;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.immobile = new Array(height);
+    for (let row = 0; row < height; row ++) 
+      this.immobile[row] = new Array(width).fill('.');
   }
 
   toString() {
     let s = '';
     for (let row = 0; row < this.height; row++) {
       for (let col = 0; col < this.width; col++) {
-        s += '.';
+        s += this.immobile[row][col]
       }
       s += "\n";
     }
