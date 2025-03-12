96bc6b1aae09f87c104fb54ff73065c6ad6fc986
Yakovyakov <yyak0423@gmail.com>
2025-03-12 14:01:04 -0400

    level5: Write Test 'it cannot be moved right beyond the board' (RED)

--- test/MovingFallingTetrominoes.test.mjs
+++ test/MovingFallingTetrominoes.test.mjs
@@ -85,4 +85,29 @@ describe("Moving Falling tetrominoes", () => {
        ..........`,
     );
   });
+
+  test("it cannot be moved right beyond the board", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveRight();
+    board.moveRight();
+    board.moveRight();
+    expect(board.toString()).to.equalShape(
+      `........T.
+       .......TTT
+       ..........
+       ..........
+       ..........
+       ..........`,
+    );
+    board.moveRight();
+    expect(board.toString()).to.equalShape(
+      `........T.
+       .......TTT
+       ..........
+       ..........
+       ..........
+       ..........`,
+    );
+  });
+
 });
