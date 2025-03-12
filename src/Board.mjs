ddd4dbd6fbbf0d54d80a52e2a45f9c6d5e7778a3
Yakovyakov <yyak0423@gmail.com>
2025-03-12 12:01:24 -0400

    level4: Format the code

--- src/Board.mjs
+++ src/Board.mjs
@@ -55,7 +55,9 @@ class MovableShape {
     for (let row = this.#row; row < this.#row + this.#shape.height(); row++) {
       for (let col = this.#col; col < this.#col + this.#shape.width(); col++) {
         const block = this.blockAt(row, col);
-        if (block !== EMPTY) points.push({ row: row, col: col });
+        if (block !== EMPTY) {
+          points.push({ row: row, col: col });
+        }
       }
     }
     return points;
