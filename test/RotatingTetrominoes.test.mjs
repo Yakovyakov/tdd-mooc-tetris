7b238293f2faf19d45538326aef765f960b3d934
Yakovyakov <yyak0423@gmail.com>
2025-03-18 11:50:48 -0400

    level7: Migrating Test for ARS (RotatingTerominoes I_SHAPE 1)
    initial orientation (GREEN)

--- test/RotatingTetrominoes.test.mjs
+++ test/RotatingTetrominoes.test.mjs
@@ -277,5 +277,39 @@ describe('Rotating Terminoes in Arika Rotation System', () => {
     });
     
   });
+
+  describe("The I shape", () => {
+    const shape = Tetromino.I_SHAPE;
+  
+    test("initial orientation", () => {
+      expect(shape.toString()).to.equalShape(
+        `....
+         IIII
+         ....
+         ....`,
+      );
+    });
+    /*
+    test("can be rotated right/clockwise", () => {
+      expect(shape.rotateRight().toString()).to.equalShape(
+        `..I..
+         ..I..
+         ..I..
+         ..I..
+         .....`,
+      );
+    });
+  
+    test("can be rotated left/counter-clockwise", () => {
+      expect(shape.rotateLeft().toString()).to.equalShape(
+        `..I..
+         ..I..
+         ..I..
+         ..I..
+         .....`,
+      );
+    });
+    */
+  });
   
 });
