02a2f1ea832e1e574a6298e73efb6b2fac714352
Yakovyakov <yyak0423@gmail.com>
2025-03-19 10:29:59 -0400

    level8: Write a Test 'a falling Tetrominoes can clear a single line when full fill rows'
    GREEN

--- test/ClearLines.test.mjs
+++ test/ClearLines.test.mjs
@@ -2,10 +2,41 @@ import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 
 import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+
+import { Tetromino } from "../src/Tetromino.mjs";
+
+import { OldTetrominoesRotationsRules } from "./OldTetrominoesRotationsRules.mjs";
+
+function fallToBottom(board) {
+  for (let i = 0; i < 10; i++) {
+    board.tick();
+  }
+}
+
+function moveToLeftWall(board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveLeft();
+  }
+}
+
+function moveToRightWall(board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveRight();
+  }
+}
+
+function moveStepsDown(board, stepts) {
+  for (let i = 0; i < stepts; i++) {
+    board.moveDown();
+  }
+}
 
 describe("Clear Lines", () => {
 
   let board;
+
   beforeEach(() => {
     board = new Board(10, 6);
   });
@@ -33,4 +64,44 @@ describe("Clear Lines", () => {
        2222..2222`,
     );
   });
+
+});
+
+describe("Clear lines when falling a Tetrominoes", () => {
+
+  let board;
+
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  test("a falling Tetrominoes can clear a single lines when full fill rows", () => {
+    board.loadFromString(
+      `..........
+       ..........
+       ..........
+       ..........
+       XXXXXXXX..
+       XXXXXXX.X.`,
+    );
+    board.drop(Tetromino.T_SHAPE);
+    board.moveDown();
+    board.rotateRight();
+    moveToRightWall(board);
+    fallToBottom(board);
+    expect(
+      board.toString(),
+      "a board have to clean rows 2",
+    ).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ..........
+       .........T
+       XXXXXXX.XT`,
+    );
+
+
+  });
+
 });
