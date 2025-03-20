42ec1bfd15d8fad81379af9d677d683501108429
Yakovyakov <yyak0423@gmail.com>
2025-03-20 10:06:22 -0400

    level10: Write Property-based test
    The order of the pieces must be different after refilling
    
    GREEN

--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -43,4 +43,23 @@ describe("ShuffleBag Tests", () => {
     shuffleBag.next();
     expect(shuffleBag._getBagLen()).to.equal(elements.length - 1);
   });
+
+  // TODO: Property-based tests
+  describe("ShuffleBag Property-based tests", () => {
+    
+    test("The order of the pieces must be different after refilling", () => {
+      const shuffleBag = new ShuffleBag(elements);
+
+      const firstOrder = [];
+      for (let i = 0; i < elements.length; i++) {
+        firstOrder.push(shuffleBag.next());
+      }
+
+      const secondOrder = [];
+      for (let i = 0; i < elements.length; i++) {
+        secondOrder.push(shuffleBag.next());
+      }
+      expect(firstOrder).not.toEqual(secondOrder);
+    });
+  });
 });
